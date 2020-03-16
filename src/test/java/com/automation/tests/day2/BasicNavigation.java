76245bcd76abf2ef285b45752950b33dee9a24fa
Bekir Inalhan <binalhan_63@hotmail.com>
2020-03-16 04:19:08 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -9,13 +9,19 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
 
         driver.get ("http://google.com");
+        Thread.sleep(3000);//for demo, wait 3 seconds
 
+        driver.close();
+        String title = driver.getTitle();//returns <title>Some title</title> text
+        System.out.println("Title is..."+title);
+        driver.close();//to close browser
+        //browser cannot close itself
     }
 
 
