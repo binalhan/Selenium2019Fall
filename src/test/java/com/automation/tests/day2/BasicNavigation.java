f47926237d5767feb0450cd7fea7b3688a71ed69
Bekir Inalhan <binalhan_63@hotmail.com>
2020-03-17 02:24:51 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -15,6 +15,10 @@ public class BasicNavigation {
         WebDriver driver = new ChromeDriver();
 
         driver.get ("http://google.com");
+
+
+         driver.manage().window().maximize();
+        driver.manage().window().fullscreen();
         Thread.sleep(3000);//for demo, wait 3 seconds
 
         String title = driver.getTitle();//returns <title>Some title</title> text
@@ -32,10 +36,22 @@ String expectedTitle = " Google";
             System.out.println("TEST FAILED");
         }
 
-
+driver.navigate() .to("http://amazon.com");
+        if(driver.getTitle().toLowerCase().contains("amazon")) {
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED!");
+        }
         driver.close();
         //browser cannot close itself
     }
+public static void verifyEquals(String arg1,String arg2) {
+        if(arg1.equals(arg2)){
+            System.out.println("test passed");
 
+        }else {
+            System.out.println("test failed");
+        }
+}
 
 }
