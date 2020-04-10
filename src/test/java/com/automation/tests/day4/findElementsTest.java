2be4861705dda59c7cdc28f4569a9f685843da7d
Bekir Inalhan <binalhan_63@hotmail.com>
2020-04-10 01:35:48 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day4/findElementsTest.java
@@ -0,0 +1,26 @@
+package com.automation.tests.day4;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class findElementsTest {
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/");
+Thread.sleep(3000);
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        for(WebElement link: links) {
+            System.out.println(link.getText());
+
+        }
+        driver.quit();
+    }
+
+}
