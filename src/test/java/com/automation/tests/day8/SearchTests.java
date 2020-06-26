3cc9366af5ecefabf263fd651903e0301d9f565f
Bekir Inalhan <binalhan_63@hotmail.com>
2020-06-26 05:00:10 -0500

    Day7 locator

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -1,13 +1,35 @@
 package com.automation.tests.day8;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.List;
+
 public class SearchTests {
     private WebDriver driver;
+
+    public void googleSearchTest() throws Exception {
+        driver.get("http/://google.com");
+        driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
+        BrowserUtils.wait(2);
+        List<WebElement>  searchItems = driver.findElements((By.tagName("h3")));
+        for(WebElement searchItem: searchItems) {
+            String var = searchItem.getText();
+            if(!var.isEmpty()){
+                System.out.println(var);
+                Assert.assertTrue(var.toLowerCase().contains("java"));
+            }
+        }
+    }
+
 
     @BeforeMethod
     public void setup(){
