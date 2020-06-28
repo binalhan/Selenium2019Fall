2c3ea48d86cc955a8194833047f6ad715885fb82
Bekir Inalhan <binalhan_63@hotmail.com>
2020-06-28 02:55:39 -0500

    Day 8

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -6,16 +6,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class SearchTests {
     private WebDriver driver;
-
+@Test
     public void googleSearchTest() throws Exception {
         driver.get("http/://google.com");
         driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
@@ -34,6 +36,9 @@ public class SearchTests {
     @BeforeMethod
     public void setup(){
         WebDriverManager.chromedriver().version("79").setup();
+        driver = new ChromeDriver();
+
+
     }
 
 
