ea0c2791cf6a5193b217f91e1d59079ea9d59b9c
Bekir Inalhan <binalhan_63@hotmail.com>
2020-06-30 01:22:48 -0500

    Day 8

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -7,45 +7,34 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.util.List;
 
 public class SearchTests {
-    private WebDriver driver;
-@Test
+   private WebDriver driver;
+    @Test
     public void googleSearchTest() throws Exception {
-        driver.get("http/://google.com");
+        driver.get("http://google.com");
         driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
         BrowserUtils.wait(2);
-        List<WebElement>  searchItems = driver.findElements((By.tagName("h3")));
-        for(WebElement searchItem: searchItems) {
-            String var = searchItem.getText();
-            if(!var.isEmpty()){
-                System.out.println(var);
-                Assert.assertTrue(var.toLowerCase().contains("java"));
-            }
+        //since every search item has a tag name <h3>
+        //it's the easiest way to collect all of them
+        List<WebElement> searchItems = driver.findElements(By.tagName("h3"));
+        for(WebElement searchItem: searchItems){
+            System.out.println(searchItem.getText());
         }
     }
-
-
     @BeforeMethod
     public void setup(){
+        //setup webdriver
         WebDriverManager.chromedriver().version("79").setup();
         driver = new ChromeDriver();
-
-
     }
-
-
     @AfterMethod
     public void teardown(){
-     driver.quit();
-
-}
-
+        //close browser and destroy webdriver object
+        driver.quit();
+    }
 }
